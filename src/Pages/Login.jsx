cb84f29745673c60ff94bdc9f1a9581c2f38a7c2
sinclaire-white <shahriyarsifat2@gmail.com>
2025-05-22 08:23:43 +0600

    login page created

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { NavLink } from "react-router";
+
+
 
 const Login = () => {
-    return (
-        <div>
-            <p>Login here</p>
-        </div>
-    );
+  return (
+    <div className="h-screen mt-40">
+     
+        <form>
+            
+         <fieldset className="p-4 mx-auto border fieldset bg-secondary border-base-300 rounded-box w-xs">
+        
+<h2 className="mb-5 text-5xl font-semibold text-center">Login Here</h2>
+        <label className="font-bold label">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="font-bold label">Password</label>
+        <input type="password" className="input" placeholder="Password" />
+
+        <button className="mt-4 btn btn-neutral text-secondary">Login</button>
+        <p>Haven't registered yet? <NavLink to={"/signup"} className={"underline"}>Sign Up Now</NavLink></p>
+      </fieldset>
+     </form>
+     
+    </div>
+  );
 };
 
 export default Login;
