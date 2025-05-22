a469d12235e37d97965e42a390a037179e2e0fae
sinclaire-white <shahriyarsifat2@gmail.com>
2025-05-22 08:31:03 +0600

    signup page created

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,27 +1,28 @@
 import { NavLink } from "react-router";
 
-
-
 const Login = () => {
   return (
     <div className="h-screen mt-40">
-     
-        <form>
-            
-         <fieldset className="p-4 mx-auto border fieldset bg-secondary border-base-300 rounded-box w-xs">
-        
-<h2 className="mb-5 text-5xl font-semibold text-center">Login Here</h2>
-        <label className="font-bold label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
+      <form>
+        <fieldset className="p-4 mx-auto border fieldset bg-secondary border-base-300 rounded-box w-xs">
+          <h2 className="mb-5 text-5xl font-semibold text-center">
+            Login Here
+          </h2>
+          <label className="font-bold label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
 
-        <label className="font-bold label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
+          <label className="font-bold label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
 
-        <button className="mt-4 btn btn-neutral text-secondary">Login</button>
-        <p>Haven't registered yet? <NavLink to={"/signup"} className={"underline"}>Sign Up Now</NavLink></p>
-      </fieldset>
-     </form>
-     
+          <button className="mt-4 btn btn-neutral text-secondary">Login</button>
+          <p>
+            Haven't registered yet?{" "}
+            <NavLink to={"/signup"} className={"underline"}>
+              Sign Up Now
+            </NavLink>
+          </p>
+        </fieldset>
+      </form>
     </div>
   );
 };
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import { NavLink } from "react-router";
 
 const SignUp = () => {
-    return (
-        <div>
-            <p>Sign up here</p>
+     return (
+        <div className="h-screen mt-40">
+          <form>
+            <fieldset className="p-4 mx-auto border fieldset bg-secondary border-base-300 rounded-box w-xs">
+              <h2 className="mb-5 text-5xl font-semibold text-center">
+                Sign Up
+              </h2>
+              <label className="font-bold label">Name</label>
+              <input type="text" className="input" placeholder="Name" />
+              <label className="font-bold label">Photo URL</label>
+              <input type="text" className="input" placeholder="Photo URL" />
+              <label className="font-bold label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+    
+              <label className="font-bold label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+    
+              <button className="mt-4 btn btn-neutral text-secondary">Sign Up</button>
+              <p>
+                Already registered?    
+                <NavLink to={"/login"} className={"underline ml-1"}>
+                    Login Now
+                </NavLink>
+              </p>
+            </fieldset>
+          </form>
         </div>
-    );
+      );
 };
 
 export default SignUp;
